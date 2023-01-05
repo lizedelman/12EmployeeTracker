9d25fa514c9a5a605cb72732b9af83c2450b0983
Wild Carrot <edelman.eliz@gmail.com>
2023-01-05 11:41:25 -0700

    pull more code from class projects to begin with

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const cTable = require("console.table");
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const db = mysql.createConnection(
   {
@@ -12,6 +18,16 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+// Query database
+db.query("SELECT * FROM students", function (err, results) {
+  console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
